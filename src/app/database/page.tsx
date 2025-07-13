85a42f527462c0242f3ba2df6e84a78c85b4bcb6
katboz <136274505+katboz@users.noreply.github.com>
2025-07-13 00:50:59 +0000

    database set, but unfortunatelly not working

---
+++ src/app/database/page.tsx
@@ -0,0 +1,33 @@
+import { MongoClient, ServerApiVersion } from 'mongodb'
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://katarina:kCxmhtpIrRC6OBDc@cluster0.ohyftis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+      // Querying our database
+    const cursor = await client.db("test").collection("greetings").find();
+    const array = await cursor.toArray()
+    return array;
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+export default async function Database() {
+    const greetings =  await run();
+    return (<>
+        {greetings.map(greetingObj=> <h1>{greetingObj.greeting}</h1>)}
+    </>)
+  }
