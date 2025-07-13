2ef79aeb6918c1965d35774bc0f9f4043a8eff08
katboz <136274505+katboz@users.noreply.github.com>
2025-07-13 10:33:52 +0000

    no sensitive information

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://katarina:kCxmhtpIrRC6OBDc@cluster0.ohyftis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://katarina:<password>@cluster0.ohyftis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
